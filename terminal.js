5ef70ac4d603ee1ae1b9eb12821f81ef2efcf66b
LeeFCash <leecash133@gmail.com>
2023-02-02 13:53:29 -0500

    made it look better

--- terminal.js
+++ terminal.js
@@ -2,12 +2,11 @@
  var aboutCreater = document.getElementById('aboutCreater');
 // getting resume
  var resume = document.getElementById('resume');
- // getting resume-homeBotton
- var resumeHomeBotton = document.getElementById('resume-homeBotton');
 // keep at bottem
- resume.remove();
- setTimeout(into, 50000);
+ //resume.remove();
+ aboutCreater.remove();
+ /*setTimeout(into, 50000);
  function into() {
     aboutCreater.remove();
     document.body.appendChild(resume);
- }
+ }*/
