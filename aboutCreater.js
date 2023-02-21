c2728c5bf1b2bdbf28271f9c40de107156311f5c
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-02-21 23:31:59 +0000

    adding a self made logo or something else

---
+++ aboutCreater.js
@@ -0,0 +1,6 @@
+var aboutCreaterAnimationCanvas = document.getElementById("aboutCreater-animation-canvas");
+var aboutCreaterAnimation = document.getElementById("aboutCreater-animation");
+
+var canvasContext = aboutCreaterAnimationCanvas.getContext("2d");
+
+canvasContext.fillReact(0, 0, 10, 1000);
