e1f4f6158de05b3d35a6981e9885a31c587f7964
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-03-16 22:57:01 +0000

    made better

--- aboutCreater.js
+++ aboutCreater.js
@@ -1,8 +1,9 @@
 alert("some phones or tablets may not show this website properly");
-//
+/*
 var aboutCreaterAnimationCanvas = document.getElementById("aboutCreater-animation-canvas");
 var aboutCreaterAnimation = document.getElementById("aboutCreater-animation");
 
 var canvasContext = aboutCreaterAnimationCanvas.getContext("2d");
 
 canvasContext.fillReact(0, 0, 10, 1000);
+*/
--- resume.js
+++ resume.js
@@ -1,3 +1,4 @@
 function send1() {
-alert('copy this -->walters_trae@hcde.org<-- then past where email goes');
+alert("if you have a ad blocker it may stop this from working");
+alert('copy this --> walters_trae@hcde.org <-- then past where email goes');
 }
