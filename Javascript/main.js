20be0942c8fd3a5015f098d1f90657786389f924
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-04-29 21:28:46 +0000

    working on new method to make animation on words test

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,12 @@
 //alert("some phones or tablets may not show this website properly");
+var mainContentAboutMeContentText3Letter1 = document.getElementById("main-content-aboutMeContent-text3-letter1");
+//var g = mainContentAboutMeContentText3Letter1;
+var tests = "soon to graduation";
+//var s = mainContentAboutMeContentText3Letter1.textContent;
+//var s = test.textContent;
+var split = tests.split("");
+console.log(split);
+
+for(var i = 0; i < split.length; i++){
+    mainContentAboutMeContentText3Letter1.innerHTML = "<span>" + split[i] + "</span>";
+}
