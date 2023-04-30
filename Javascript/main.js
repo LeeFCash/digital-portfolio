0f7d892acfa51bdf54e7137fab423b3b71c5dfe6
Lee Cash <118697195+LeeFCash@users.noreply.github.com>
2023-04-30 20:13:24 +0000

    made fade new

--- Javascript/main.js
+++ Javascript/main.js
@@ -5,5 +5,23 @@ var split = v.split("");
 console.log(split);
 
 for(let i = 0; i < split.length; i++){
-    mainContentAboutMeContentText3Letters.innerHTML += "<span class=\"hello\">" + split[i] + "</span>";
+    mainContentAboutMeContentText3Letters.innerHTML += "<span class=\"main-content-aboutMeContent-text3-letter"+[i]+"-opacity0\">" + split[i] + "</span>";
+}
+
+setTimeout(setTimer, 5000);
+
+function setTimer() {
+var letter = 0;
+var timer = setInterval(ontick, 150);
+
+function ontick() {
+    var letterSlector = mainContentAboutMeContentText3Letters.querySelectorAll('span')[letter];
+    letterSlector.style.opacity = "1.5";
+    letter++;
+    if(letter === split.length){
+        clearInterval(timer);
+        timer = null;
+        return;
+    }
+}
 }
