9bcddc2f7a54725a5e9baee89ca4a761279fc53d
LeeFCash <leecash133@gmail.com>
2023-06-27 18:17:42 -0400

    working on tab stuff

--- Javascript/workSamplesTab/workSamplesTab.js
+++ Javascript/workSamplesTab/workSamplesTab.js
@@ -4,6 +4,14 @@ var tabWorkSamples = document.getElementById("main-tabBar-tab2");
 tabWorkSamples.addEventListener("click", openTab);
 
 function openTab() {
-    var aboutMe = document.getElementById("main-content-aboutMeContent");
-    aboutMe.remove();
+    var aboutMeTab = document.getElementById("main-tabBar-aboutMeTab");
+    var acontactMeTab = document.getElementById("main-tabBar-contactMeTab");
+    var aboutMeC = document.getElementById("main-content-aboutMeContent");
+    if(aboutMeTab === null) {
+        aboutMeTab.removeAttribute('id', 'main-tabBar-tab-select');
+    }
+    if(acontactMeTab === null) {
+        acontactMeTab.removeAttribute('id', 'main-tabBar-tab-select');
+    }
+    aboutMeC.remove();
 }
