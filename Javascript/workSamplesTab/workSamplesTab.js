7ecc7fec3ff0f8e45c7fff34f913a38d04ddd009
LeeFCash <leecash133@gmail.com>
2023-06-29 17:01:53 -0400

    worked on new tab tab2

--- Javascript/workSamplesTab/workSamplesTab.js
+++ Javascript/workSamplesTab/workSamplesTab.js
@@ -5,13 +5,20 @@ tabWorkSamples.addEventListener("click", openTab);
 
 function openTab() {
     var aboutMeTab = document.getElementById("main-tabBar-aboutMeTab");
-    var acontactMeTab = document.getElementById("main-tabBar-contactMeTab");
+    var contactMeTab = document.getElementById("main-tabBar-contactMeTab");
     var aboutMeC = document.getElementById("main-content-aboutMeContent");
-    if(aboutMeTab === null) {
-        aboutMeTab.removeAttribute('id', 'main-tabBar-tab-select');
-    }
-    if(acontactMeTab === null) {
-        acontactMeTab.removeAttribute('id', 'main-tabBar-tab-select');
-    }
+    var mainContent = document.getElementById("main-content");
+    var mainTabBarList = document.getElementById("main-tabBar-list");
+    var mainContentWorkSemplesContent = document.createElement("div");
+    var aboutMeTabRemove = mainTabBarList.querySelectorAll('button')[0];
+    var tab1 = document.createElement("button");
+    tab1.innerHTML = "About me";
+    tab1.setAttribute("class", "main-tabBar-tab1");
+    //tabWorkSamples.setAttribute("id", "main-tabBar-tab-select");
+    mainTabBarList.removeChild(aboutMeTabRemove);
+    //contactMeTab.remove();
     aboutMeC.remove();
+    mainTabBarList.appendChild(tab1);
+    mainContentWorkSemplesContent.setAttribute("class", "main-content-WorkSemplesContent");
+    mainContent.appendChild(mainContentWorkSemplesContent);
 }
